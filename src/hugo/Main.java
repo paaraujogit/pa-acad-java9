568c1030f0d7ba07250d527efd0c866c81a79e34
Administrator <Administrator@10.5.3.168>
2019-09-18 19:24:02 +0100

    Commit inicial

--- src/hugo/Main.java
+++ src/hugo/Main.java
@@ -3,7 +3,7 @@ package hugo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello there.");
+		System.out.println("Hello there. Testing Git.....");
 	}
 
 }
