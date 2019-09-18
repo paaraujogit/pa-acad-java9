a220277cc87b39f185867fc6dc1bf4c8f8031b52
Administrator <Administrator@10.5.3.169>
2019-09-18 19:18:04 +0100

    Classe Ola

---
+++ src/rui/Ola.java
@@ -0,0 +1,11 @@
+package rui;
+
+public class Ola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Java, os testes estão cheios e rasteiras!!!!!!!");
+		System.out.println("Entretanto já fiz asneira no commit!");
+		}
+
+}
