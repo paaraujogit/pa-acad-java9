3cb19401d640078deac41b74fb80a95a589fb8a4
Administrator <Administrator@10.5.3.169>
2019-09-18 19:28:56 +0100

    Commit 2

--- src/rui/Ola.java
+++ src/rui/Ola.java
@@ -5,7 +5,7 @@ public class Ola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Olá Java, os testes estão cheios e rasteiras!!!!!!!");
-		System.out.println("Entretanto já fiz asneira no commit!");
+		System.out.println("Entretanto já fiz asneira no commit!  ");
 		}
 
 }
